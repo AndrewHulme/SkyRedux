8167776028641c8947830b7f52354ac8281f0e03
Andrew Hulme <andrewhulme1993@gmail.com>
2021-02-09 12:05:53 +0000

    Refactors tests with beforeEach

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,18 +1,18 @@
 describe("Search", () => {
-  it("should show a search input field and a search button", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000");
+  });
+
+  it("should show a search input field and a search button", () => {
     cy.get("#searchInput").should("be.visible");
     cy.get("#searchButton").should("be.visible");
   });
 
   it("should immediately focus on search input field when user lands on page", () => {
-    cy.visit("http://localhost:3000");
-
     cy.focused().should("have.id", "searchInput");
   });
 
   it("should show search results when user initiates a search", () => {
-    cy.visit("http://localhost:3000");
     cy.get("#searchInput").type("futurama");
     cy.get("#searchButton").click();
     cy.get("#results").should("exist");
--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import Search from "./search";
 import SearchInput from "./searchInput";
 import SearchButton from "./searchButton";
 
 describe("Search", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Search />);
+  });
+
   it("renders the SearchInput component", () => {
-    const wrapper = shallow(<Search />);
     const searchInput = wrapper.find(SearchInput);
     expect(searchInput.exists()).toBe(true);
   });
 
   it("renders the SearchButton component", () => {
-    const wrapper = shallow(<Search />);
     const searchButton = wrapper.find(SearchButton);
     expect(searchButton.exists()).toBe(true);
   });
