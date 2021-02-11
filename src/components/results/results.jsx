4c353b6b77bdd338b25bcf83437e118ab11d8a40
Andrew Hulme <andrewhulme1993@gmail.com>
2021-02-11 09:23:31 +0000

    Converts results page to a table

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -3,18 +3,28 @@ import React from "react";
 function Results(props) {
   return (
     <div id="results">
-      {props.results.map((result) => (
-        <div
-          id={"id" + result.id}
-          className="resultsGrid"
-          key={result.id}
-          onClick={() => props.onDetails(result)}
-        >
-          {result.media_type}
-          {": "}
-          {result.media_type === "movie" ? result.title : result.name}
-        </div>
-      ))}
+      <table>
+        <tbody>
+          <tr>
+            <th>Media Type</th>
+            <th>Name/Title</th>
+          </tr>
+
+          {props.results.map((result) => (
+            <tr
+              id={"id" + result.id}
+              className="resultsGrid"
+              key={result.id}
+              onClick={() => props.onDetails(result)}
+            >
+              <td>{result.media_type}</td>
+              <td>
+                {result.media_type === "movie" ? result.title : result.name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
